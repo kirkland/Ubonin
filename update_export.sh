3ef1ea75d11f3208160824e196572faea19e4c55
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-10-26 20:55:39 -0400

    some update

---
+++ update_export.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+  echo "usage: $0 <filename.zip>"
+  exit 1
+fi
+
+if [[ -d temp ]]; then
+  rm -rf temp
+fi
+mkdir temp
+
+unzip "$1" -d temp
+
+dirs=( layout templates config assets )
+for dir in ${dirs[@]}; do
+  mv temp/ubonin-myshopify-com-main/$dir/* $dir
+done
